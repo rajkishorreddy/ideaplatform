bf291e07bae7a1695510568c339aa12fa3fbcc15
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-05-15 23:31:23 +0530

    footer changed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-name">
+      {/* <div className="footer-name">
         <div> RAKESH BANDI - rakeshbandi2000@gmail.com</div>
         <div>A Mahesh -</div>
-      </div>
+      </div> */}
     </div>
   );
 };
